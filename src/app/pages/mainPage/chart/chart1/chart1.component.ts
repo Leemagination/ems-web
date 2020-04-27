2059ef2bf227db75979eca0d2c378f95cca234fa
lihuwen <lihuwen@banggood.com>
2020-04-27 17:59:25 +0800

    图表页面开发

--- src/app/pages/mainPage/chart/chart1/chart1.component.ts
+++ src/app/pages/mainPage/chart/chart1/chart1.component.ts
@@ -1,17 +1,48 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import echarts from 'echarts';
+import { debounce, debounceTime } from 'rxjs/operators';
+import { Observable, of, pipe, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chart1',
   templateUrl: './chart1.component.html',
   styleUrls: ['./chart1.component.scss']
 })
-export class Chart1Component implements OnInit, OnDestroy {
-  chart1Test = '';
+export class Chart1Component implements OnInit, AfterViewInit, OnDestroy {
+  myChart: any;
+  subject$ = new Subject<any>();
+  @ViewChild('barChart', {static: true}) barChart: ElementRef;
+  option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [{
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar'
+    }]
+  };
+
+  @HostListener('window:resize', ['$event'])
+  windowResize($event) {
+    this.subject$.next($event);
+  }
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.subject$.pipe(debounceTime(300)).subscribe(data => {
+      this.myChart.resize();
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.myChart = echarts.init(this.barChart.nativeElement);
+    this.myChart.setOption(this.option);
   }
 
   ngOnDestroy(): void {
