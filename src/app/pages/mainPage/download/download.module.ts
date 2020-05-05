2fc86987ba51d785bb11a6bd06b4a2fe775d8f19
Leemagination <328445543@qq.com>
2020-05-05 18:16:55 +0800

    增加下载演示

--- src/app/pages/mainPage/download/download.module.ts
+++ src/app/pages/mainPage/download/download.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { DownloadRoutingModule } from './download-routing.module';
 import { DownloadComponent } from './download.component';
+import { ShareModule } from '../../../share/share.module';
 
 
 @NgModule({
   declarations: [DownloadComponent],
   imports: [
     CommonModule,
+    ShareModule,
     DownloadRoutingModule
   ]
 })
-export class DownloadModule { }
+export class DownloadModule {
+}
