e2d5c98e112a691d15c04b737cc5e0c1d632f9e1
Leemagination <328445543@qq.com>
2020-05-14 17:54:57 +0800

    localStorage添加初始数据

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,39 @@ export class AppComponent {
         editable: false
       }, {address: '上海', editable: false}, {address: '北京', editable: false}]));
     }
+    if (!localStorage.getItem('tableMockData')) {
+      const data = [{
+        'area': ['110000', '110101'],
+        'status': true,
+        'orderType': '发货单',
+        'deliverStorage': '广州',
+        'address': '冲鸭鸭鸭鸭鸭',
+        'remark': '这就是一个备注而已',
+        'orderID': '123456789',
+        'createName': '小李',
+        'createDate': '2020-05-14T09:37:36.529Z'
+      }, {
+        'area': ['440000', '440100', '440111'],
+        'status': false,
+        'orderType': '退货单',
+        'deliverStorage': '上海',
+        'address': '随便填一个地址',
+        'remark': '',
+        'orderID': '987654321',
+        'createName': '小李',
+        'createDate': '2020-05-14T09:38:23.640Z'
+      }, {
+        'area': ['510000', '510100', '510114'],
+        'status': true,
+        'orderType': '出库单',
+        'deliverStorage': '深圳',
+        'address': 'XX区XXX路 XX 号',
+        'remark': '11111',
+        'orderID': '66666666',
+        'createName': '小李',
+        'createDate': '2020-05-14T09:39:05.076Z'
+      }];
+      localStorage.setItem('tableMockData', JSON.stringify(data));
+    }
   }
 }
